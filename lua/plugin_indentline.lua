7e10e9890aa2ca577de10766bd1a8619d5c3273d
Guangzong Chen <chen-gz@outlook.com>
2023-10-19 15:32:02 -0400

    update

--- lua/plugin_indentline.lua
+++ lua/plugin_indentline.lua
@@ -2,11 +2,11 @@ local use = require('packer').use
 use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-        require("indent_blankline").setup {
-            show_current_context = true,
-            show_current_context_start = true,
-        }
+        -- require("indent_blankline").setup {
+        --     show_current_context = true,
+        --     show_current_context_start = true,
+        -- }
+        -- migrate to v3    
+        require("ibl").setup()
     end
-
-
 }
