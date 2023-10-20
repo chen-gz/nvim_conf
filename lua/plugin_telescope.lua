5f7543c6d4cef3c0e590bdf780fc718398d50a55
Guangzong Chen <chen-gz@outlook.com>
2023-10-20 00:15:09 -0400

    merge

--- lua/plugin_telescope.lua
+++ lua/plugin_telescope.lua
@@ -1,5 +1,5 @@
 local use = require('packer').use
- use {
+use {
     'nvim-telescope/telescope-fzf-native.nvim',
     run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
 }
