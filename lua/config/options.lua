4226d7c15e58549d66539449998d49df46ad2dcb
Mason <mason@ggeta.com>
2023-11-16 23:14:47 -0500

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 -- allow cursor move to everywhere in normal mode
 vim.o["virtualedit"] = "all"
+vim.o.guifont = "Source Code Pro:h21" -- text below applies for VimScript
+-- set tab size to 4 spaces
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
