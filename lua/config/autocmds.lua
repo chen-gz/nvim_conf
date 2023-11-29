17320b52764d1828f5c6f14ef4031fb660694b15
Mason <mason@ggeta.com>
2023-11-29 16:58:14 -0500

    update auto cmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,33 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = { "*.tex" },
+--   callback = function()
+--     -- Save the current cursor position
+--     local cur_pos = vim.api.nvim_win_get_cursor(0)
+--
+--     -- Set the text width and format the text
+--     vim.opt_local.textwidth = 80
+--     vim.cmd("normal gggqG")
+--
+--     -- Restore the cursor position
+--     vim.api.nvim_win_set_cursor(0, cur_pos)
+--   end,
+-- })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.tex" },
+  callback = function()
+    -- Save the current view state
+    local saved_view = vim.fn.winsaveview()
+
+    -- Set the text width and format the text
+    vim.opt_local.textwidth = 80
+    vim.cmd("normal gggqG")
+
+    -- Restore the view state
+    vim.fn.winrestview(saved_view)
+  end,
+})
